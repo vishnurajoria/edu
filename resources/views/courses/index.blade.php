8fe8f58031276b19c20decc670282396ee3bff25
Marius <marius.buraga89@yahoo.com>
2017-04-18 11:44:56 +0300

    Wider home page. Enrolled courses before all courses

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -3,6 +3,45 @@
     <div class="container">
         <div class="row">
             <div class="col-md-9 col-md-offset-1">
+                @if(Auth::check())
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">Enrolled in</div>
+                                <div class="panel-body">
+                                    @if(count($user_courses))
+                                        <ul>
+                                            @foreach($user_courses as $course)
+                                                <li><a href="/courses/{{$course->id}}">{{$course->title}}</a></li>
+                                            @endforeach
+                                        </ul>
+                                    @else
+                                        <h3>No courses</h3>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">Enrolled in by group</div>
+                                <div class="panel-body">
+                                    @if(count($user_courses_by_group))
+                                        <ul>
+                                            @foreach($user_courses_by_group as $course)
+                                                <li><a href="/courses/{{$course->id}}">{{$course->title}}</a></li>
+                                            @endforeach
+                                        </ul>
+                                    @else
+                                        <h3>No courses</h3>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('teacher'))
+                        <a class="btn btn-primary" href="/courses/create" style="margin-bottom: 30px;">Create new course</a>
+                    @endif
+                @endif
                 <div class="panel panel-default">
                     <div class="panel-heading"><h1>All Courses
                             @if(isset(request()->input()['page']))
@@ -27,45 +66,7 @@
                     </div>
                     <div class="text-center">{{ $courses->links() }}</div>
                 </div>
-                @if(Auth::check())
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">Enrolled in</div>
-                                <div class="panel-body">
-                                    @if(count($user_courses))
-                                        <ul>
-                                        @foreach($user_courses as $course)
-                                            <li><a href="/courses/{{$course->id}}">{{$course->title}}</a></li>
-                                        @endforeach
-                                        </ul>
-                                    @else
-                                        <h3>No courses</h3>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">Enrolled in by group</div>
-                                <div class="panel-body">
-                                    @if(count($user_courses_by_group))
-                                        <ul>
-                                        @foreach($user_courses_by_group as $course)
-                                            <li><a href="/courses/{{$course->id}}">{{$course->title}}</a></li>
-                                        @endforeach
-                                        </ul>
-                                    @else
-                                        <h3>No courses</h3>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('teacher'))
-                        <a class="btn btn-primary" href="/courses/create">Create new course</a>
-                    @endif
-                @endif
+
             </div>
         </div>
     </div>
